2ad261770afb8fd5397c79954bdcaf3c1d69aff4
amad87 <zulhilmi5581@gmail.com>
2019-11-20 14:45:04 +0800

    Updates

---
+++ precache-manifest.62e9a23acf44dbadc1e7516de0e15de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c328b8e5a451ba9db63a514616a9762d",
+    "url": "/user-app/index.html"
+  },
+  {
+    "revision": "0ce6f00fc82ef3d2dd17",
+    "url": "/user-app/static/css/main.2fe33c87.chunk.css"
+  },
+  {
+    "revision": "a0ba525f2d9f93fa2323",
+    "url": "/user-app/static/js/2.44fa4a56.chunk.js"
+  },
+  {
+    "revision": "0ce6f00fc82ef3d2dd17",
+    "url": "/user-app/static/js/main.0f000a46.chunk.js"
+  },
+  {
+    "revision": "809e773395835cc2a0a3",
+    "url": "/user-app/static/js/runtime-main.329ee413.js"
+  }
+]);
